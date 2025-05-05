ada52e4a14454b39b74726b2c85487790a07849a
joeemira <youssefemira1@gmail.com>
2025-05-05 15:06:17 +0300

    login process

--- auth2.py
+++ auth2.py
@@ -1,1 +1,15 @@
-from jose import    JWTError, jwt
+from jose import    jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "welcome"
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
+
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire=datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends , status, HTTPException
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from .. import models, schemas, utils
 from ..database import get_db
+from ..import auth2
+
 
 
 
@@ -11,11 +14,13 @@ router = APIRouter(
 )
 
 @router.post("/login")
-def login(user_credentials: schemas.user_login, db: Session = Depends(get_db)):
-    user =db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user =db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_FORBIDDEN,detail=f"invalid credentials")
+        raise HTTPException(status_code=status.HTTP_404_FORBIDDEN,detail="invalid credentials")
     if not utils.verify(user_credentials.password,user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"invalid credentials")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="invalid credentials")
     
-    return {"access_token":user.id }
+    access_token = auth2.create_access_token(data={"user_id":user.id}) 
+    
+    return {"access_token":access_token,"token_type":"bearer"}
